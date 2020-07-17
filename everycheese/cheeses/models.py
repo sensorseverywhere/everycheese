826800d664050ae67ed25502c27b1dab6d6cb383
wade <wade_mansell@hotmail.com>
2020-07-18 07:09:35 +1000

    added user creator to cheese detail

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
 
@@ -20,10 +21,10 @@ class Cheese(TimeStampedModel):
     slug = AutoSlugField("Cheese Address", unique=True, always_update=False, populate_from="name")
     description = models.TextField("Description", blank=True)
     firmness = models.CharField("Firmness", max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
-
     country_of_origin = CountryField(
         "Country of Origin", blank=True
     )
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
