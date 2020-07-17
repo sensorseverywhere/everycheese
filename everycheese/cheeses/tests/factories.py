3db92a9ae7d9e9cf29cbed4f2fe923bdba5d5423
wade <wade_mansell@hotmail.com>
2020-07-18 08:21:00 +1000

    fixed test

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -4,6 +4,7 @@ import factory
 import factory.fuzzy
 
 from ..models import Cheese
+from everycheese.users.tests.factories import UserFactory 
 
 
 
@@ -12,6 +13,7 @@ class CheeseFactory(factory.django.DjangoModelFactory):
     slug = factory.LazyAttribute(lambda obj: slugify(obj.name))
     description = factory.Faker('paragraph', nb_sentences=3, variable_nb_sentences=True)
     country_of_origin = factory.Faker('country_code')
+    creator = factory.SubFactory(UserFactory) 
 
     firmness = factory.fuzzy.FuzzyChoice(
         [x[0] for x in Cheese.Firmness.choices]
